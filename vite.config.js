0a55fd861f8abc2b654771b3bab75d50a703a20e
Diyorbek Ashurbekov <diyorbekashurbekov@users.noreply.github.com>
2026-09-11 02:12:32 +0600

    Add production build in docs/ for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
   plugins: [
     react(),
     tailwindcss(),
